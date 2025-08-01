6909b67b189f1c0181365de5a64743ff967cad20
dvt0000 <dave@brovan.be>
2025-08-01 08:13:13 +0200

    Improve app entry and fix config files

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+def main() -> None:
+    """Render the Streamlit UI."""
+
+    st.title("🎈 My new app")
+    st.write(
+        "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    )
+
+
+if __name__ == "__main__":
+    if not st.runtime.exists():
+        raise SystemExit(
+            "Please run this app using 'streamlit run streamlit_app.py'"
+        )
+    main()
+else:
+    main()
